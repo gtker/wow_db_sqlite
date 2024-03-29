a55df8c3eb8b09baac22f856e36d3c6b87b143f4
Gtker <git@gtker.com>
2024-03-29 16:50:15 +0100

    Add csv for everything but spells/items

--- update.py
+++ update.py
@@ -4,20 +4,14 @@ import subprocess
 import os
 import zipfile
 import urllib.request
+import argparse
 
 WOW_MESSAGES_REPO = "https://github.com/gtker/wow_messages/"
-EXPANSIONS = ["classic", "tbc", "wotlk"]
-
-
-def cleanup():
-    for expansion in EXPANSIONS:
-        for file in [f"{expansion}-sqlite-db.zip", f"{expansion}logs.sqlite", f"{expansion}mangos.sqlite", f"{expansion}realmd.sqlite", f"{expansion}characters.sqlite"]:
-            if os.path.isfile(file):
-                os.remove(file)
+EXPANSIONS = [("classic", "vanilla"), ("tbc", "tbc"), ("wotlk", "wrath")]
 
 
 def download_databases():
-    for expansion in EXPANSIONS:
+    for expansion, expac in EXPANSIONS:
         file_name = f"{expansion}-sqlite-db.zip"
 
         print(f"Downloading '{file_name}'")
@@ -34,17 +28,38 @@ def download_databases():
         print(f"Renaming '{sqlite_file}' to '{expansion}.sqlite'")
         os.rename(sqlite_file, f"{expansion}.sqlite")
 
-        zip_file = f"{expansion}.zip"
-        if os.path.isfile(zip_file):
-            os.remove(zip_file)
-        with zipfile.ZipFile(zip_file, 'w', compression=zipfile.ZIP_LZMA) as zip:
-            zip.write(f"{expansion}.sqlite")
+        extract_sqlites(sqlite_file, expac)
+
+
+def extract_sqlites(sqlite_file: str, expac: str):
+    def run_sqlite(file:str, query: str):
+        subprocess.run(["sqlite3", sqlite_file, '.headers on', '.mode csv', f'.once {expac}/{file}.csv', query])
+
+    run_sqlite("pet_names", "SELECT * from pet_name_generation")
+    run_sqlite("tavern_triggers", "SELECT * from areatrigger_tavern;")
+    run_sqlite("quest_triggers", "SELECT * FROM areatrigger_involvedrelation;")
+    run_sqlite("teleport_triggers", "SELECT * FROM areatrigger_teleport;")
+    run_sqlite("actions", "SELECT * FROM playercreateinfo_action;")
+    run_sqlite("distinct_actions", "SELECT DISTINCT race, class FROM playercreateinfo_action ORDER BY race, class;")
+    run_sqlite("level_exp", "select lvl level, xp_for_next_level exp from player_xp_for_level ORDER BY level;")
+    run_sqlite("exploration_exp", "select level, basexp exp from exploration_basexp where level != 0 ORDER BY level;")
+    run_sqlite("stat_data", "SELECT race, l.class, l.level, str, agi, sta, inte, spi, basehp, basemana FROM player_levelstats l LEFT JOIN player_classlevelstats c on l.level = c.level and l.class = c.class;")
+    run_sqlite("skills", "SELECT * FROM playercreateinfo_skills ORDER BY skill;")
+    run_sqlite("initial_spells", "SELECT * FROM playercreateinfo_spell;")
+
 
 def main():
-    download_databases()
+    parser = argparse.ArgumentParser(prog='update.py')
+    parser.add_argument('-x', '--extract-only', action='store_true', default=False)
+    args = parser.parse_args()
+
+    if args.extract_only:
+        for expansion, expac in EXPANSIONS:
+            extract_sqlites(f"{expansion}.sqlite", expac)
+    else:
+        download_databases()
 
 
 if __name__ == '__main__':
     main()
-
 
